72c3729a02f8caa62f5e10b64626e1baefe52827
tiennvn1922 <tiennvn1922@gmail.com>
2022-10-29 16:45:28 +0700

    SQLite Injection

---
+++ Web Gauntlet 2/filter.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["winner2"])) {
+    $_SESSION["winner2"] = 0;
+}
+$win = $_SESSION["winner2"];
+$view = ($_SERVER["PHP_SELF"] == "/filter.php");
+
+if ($win === 0) {
+    $filter = array("or", "and", "true", "false", "union", "like", "=", ">", "<", ";", "--", "/*", "*/", "admin");
+    if ($view) {
+        echo "Filters: ".implode(" ", $filter)."<br/>";
+    }
+} else if ($win === 1) {
+    if ($view) {
+        highlight_file("filter.php");
+    }
+    $_SESSION["winner2"] = 0;        // <- Don't refresh!
+} else {
+    $_SESSION["winner2"] = 0;
+}
+
+// picoCTF{0n3_m0r3_t1m3_e2db86ae880862ad471aa4c93343b2bf}
+?>
---
+++ test.js
@@ -0,0 +1,7 @@
+var a = 1;
+var a = 0;
+console.log(a);
+function toCelsius(f) {
+  return (5 / 9) * (f - 32);
+}
+console.log(toCelsius);
